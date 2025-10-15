a9a21ff4313fd918de26d8d12ea3843145408301
chaeyoung <happy7yong83@gmail.com>
2025-10-15 14:47:52 +0900

    feat : userDto

---
+++ src/main/java/com/ran/community/user/dto/UserDto.java
@@ -0,0 +1,52 @@
+package com.ran.community.user.dto;
+
+public class UserDto {
+    private long userId;
+    private String email;
+    private String username;
+    private String password;
+
+    public UserDto() {}
+    public UserDto(long userId, String email, String username, String password) {
+        this.userId = userId;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId){
+        this.userId = userId;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    public String toString(){
+        return username + " " + userId + " " + email + " " + password;
+    }
+}
