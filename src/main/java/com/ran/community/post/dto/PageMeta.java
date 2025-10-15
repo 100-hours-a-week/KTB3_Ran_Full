db06883d436e1f26463d1110ddc05c66953bffbc
chaeyoung <happy7yong83@gmail.com>
2025-10-15 20:38:00 +0900

    feat : PageMeta

---
+++ src/main/java/com/ran/community/post/dto/PageMeta.java
@@ -0,0 +1,42 @@
+package com.ran.community.post.dto;
+
+public class PageMeta {
+    private int numOfContents;
+    private int limit;
+    private int numOfPages;
+    private int page;
+
+    public PageMeta() {}
+    public PageMeta(int numOfContents, int limit, int numOfPages, int page) {
+        this.numOfContents = numOfContents;
+        this.limit = limit;
+        this.numOfPages = numOfPages;
+        this.page = page;
+
+    }
+
+    public int getNumOfContents() {
+        return numOfContents;
+    }
+    public void setNumOfContents(int numOfContents) {
+        this.numOfContents = numOfContents;
+    }
+    public int getLimit() {
+        return limit;
+    }
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+    public int getNumOfPages() {
+        return numOfPages;
+    }
+    public void setNumOfPages(int numOfPages) {
+        this.numOfPages = numOfPages;
+    }
+    public int getPage() {
+        return page;
+    }
+    public void setPage(int page) {
+        this.page = page;
+    }
+}
