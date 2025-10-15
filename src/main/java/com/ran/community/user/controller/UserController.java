9519971e1f77e236edd74d1ecb2c49630a2cc6d1
chaeyoung <happy7yong83@gmail.com>
2025-10-15 14:48:17 +0900

    feat : userController

---
+++ src/main/java/com/ran/community/user/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.ran.community.user.controller;
+
+import com.ran.community.user.dto.UserDto;
+import com.ran.community.user.dto.UserLoginDto;
+import com.ran.community.user.dto.UserSignupFormDto;
+import com.ran.community.user.service.UserService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    //DI
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //회원 가입
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@Valid @RequestBody UserSignupFormDto userSignupFormDto) {
+        UserDto userDto = userService.registerUser(userSignupFormDto);
+        logger.info(userDto.toString());
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message","register_success","data",Map.of("userId",userDto.getUserId(),"username",userDto.getUsername(),"email",userDto.getEmail())));
+    }
+
+    //로그인
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody UserLoginDto userLoginDto){
+        UserDto userDto = userService.login(userLoginDto);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","login_success","data",Map.of("userId",userDto.getUserId())));
+    }
+
+    //회원 정보 조회
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> userInfo(@PathVariable Long userId){
+        UserDto userDto = userService.getUser(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","user_confirm","data",Map.of("userId",userDto.getUserId(),"username",userDto.getUsername(),"email",userDto.getEmail(),"password",userDto.getPassword())));
+    }
+
+    //회원 정보 수정
+    @PatchMapping("/{userId}")
+    public ResponseEntity<?> userPatchInfo(@Valid @PathVariable Long userId,@RequestBody UserSignupFormDto userSignupFormDto){
+        UserDto userDto = userService.updateUser(userId,userSignupFormDto);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","Patch_user","data",Map.of("userId",userDto.getUserId(),"username",userDto.getUsername(),"email",userDto.getEmail(),"password",userDto.getPassword())));
+    }
+
+    //회원 탈퇴
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> userDelete(@PathVariable Long userId){
+        UserDto userDto = userService.deletedUser(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","user_delete","data",Map.of("userId",userDto.getUserId(),"username",userDto.getUsername(),"email",userDto.getEmail(),"password",userDto.getPassword())));
+    }
+
+}
