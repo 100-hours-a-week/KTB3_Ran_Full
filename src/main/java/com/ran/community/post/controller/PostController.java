f0df25a2dfcbd01e3aedd30ab7a61e8de9a95115
chaeyoung <happy7yong83@gmail.com>
2025-10-15 20:41:46 +0900

    feat : PostController

---
+++ src/main/java/com/ran/community/post/controller/PostController.java
@@ -0,0 +1,73 @@
+package com.ran.community.post.controller;
+
+import com.ran.community.post.dto.PageDto;
+import com.ran.community.post.dto.PostCreateFormDto;
+import com.ran.community.post.dto.PostDto;
+import com.ran.community.post.service.PostService;
+import com.ran.community.user.dto.UserDto;
+import com.ran.community.user.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/posts")
+@RestController
+public class PostController {
+    private final UserService userService;
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
+    }
+
+    //게시물 생성
+    @PostMapping()
+    public ResponseEntity<?> postCreate(@Valid @RequestBody PostCreateFormDto postCreateFormDto, HttpSession httpSession) {
+        //userId의 userDto를 가져오기
+        UserDto userDto = userService.getUser((long) httpSession.getAttribute("userId"));
+        //로그인된 user의 객체도 함께 사용
+        PostDto postDto = postService.postCreate(userDto,postCreateFormDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message","posting_success","data",postDto));
+    }
+
+    //특정 게시물 조회
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> postRead(@PathVariable Long postId){
+        PostDto postDto = postService.postRead(postId);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","posting_read_success","data",postDto));
+    }
+
+    //전체 게시물 조회
+    //이거 반환값 확인 바람.
+    @GetMapping()
+    public ResponseEntity<?> postsRead(HttpSession httpSession,@RequestParam int page, @RequestParam int limit){
+        //userId가 있을 경우에만 확인
+        userService.getUser((long) httpSession.getAttribute("userId"));
+        PageDto pageData = postService.postsRead(page,limit);//페이지 네이션 되어있음.
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","posts_read_success","data",pageData.getOffsetPosts(),"meta",pageData.getPageMeta()));
+
+    }
+
+    //게시물 수정
+    @PatchMapping("/{postId}")
+    public ResponseEntity<?> updatePost(@Valid @RequestBody PostCreateFormDto postCreateFormDto, @PathVariable Long postId){
+        PostDto postDto = postService.updatePost(postId,postCreateFormDto);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","posts_patch_success","data",postDto));
+    }
+
+    //게시물 삭제
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<?> deletePost(@PathVariable Long postId){
+        PostDto postDto = postService.deletePost(postId);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","posts_delete_success","data",postDto));
+    }
+
+}
