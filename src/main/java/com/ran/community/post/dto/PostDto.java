a576800e5c14345af644616aa9fb17c08a9a9940
chaeyoung <happy7yong83@gmail.com>
2025-10-15 20:42:12 +0900

    feat : PostDto 게시글 기본 정보

---
+++ src/main/java/com/ran/community/post/dto/PostDto.java
@@ -0,0 +1,71 @@
+package com.ran.community.post.dto;
+
+import com.ran.community.user.dto.UserDto;
+
+import java.time.LocalDateTime;
+
+public class PostDto {
+    private long postId;
+    private String postTitle;
+    private String postContent;
+    private long postAuthor;//User에게서 가져오기 FK
+    private String postImageUrl;
+    private LocalDateTime postDate;
+
+    public PostDto() {}
+    public PostDto(long postId, String postTitle, String postContent, UserDto user, LocalDateTime postDate, String postImageUrl) {
+        this.postId = postId;
+        this.postTitle = postTitle;
+        this.postContent = postContent;
+        this.postAuthor = user.getUserId(); //이게 맞나?
+        this.postDate = postDate;
+        this.postImageUrl = postImageUrl;
+    }
+
+    public long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(long postId) {
+        this.postId = postId;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    public long getPostAuthor() {
+        return postAuthor;
+    }
+
+    public void setPostAuthor(long postAuthor) {
+        this.postAuthor = postAuthor;
+    }
+
+    public LocalDateTime getPostDate() {
+        return LocalDateTime.now();
+    }
+
+    public void setPostDate(LocalDateTime postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getPostImageUrl() {
+        return postImageUrl;
+    }
+    public void setPostImageUrl(String postImageUrl) {
+        this.postImageUrl = postImageUrl;
+    }
+}
