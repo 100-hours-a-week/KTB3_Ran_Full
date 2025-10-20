9e174628a8e5346a902f50684fff448f47fb7f21
happy7yong <happy7yong83@gmail.com>
2025-10-20 19:20:12 +0900

    feat : 댓글 조회 dto 추가

---
+++ src/main/java/com/ran/community/comment/dto/response/commentResponseDTO.java
@@ -0,0 +1,46 @@
+package com.ran.community.comment.dto.response;
+
+import com.ran.community.comment.entity.Comment;
+
+import java.time.LocalDateTime;
+
+public class commentResponseDTO {
+    private long commentId;
+    private String content;
+    private long authorId;
+    private LocalDateTime postTime;
+
+    public commentResponseDTO(Comment comment) {}
+    public commentResponseDTO(long commentId, String content, long authorId, LocalDateTime postTime) {
+        this.commentId = commentId;
+        this.content = content;
+        this.authorId = authorId;
+        this.postTime = postTime;
+    }
+
+    public long getCommentId() {
+        return commentId;
+    }
+    public void setCommentId(long commentId) {
+        this.commentId = commentId;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public long getAuthorId() {
+        return authorId;
+    }
+    public void setAuthorId(long authorId) {
+        this.authorId = authorId;
+    }
+    public LocalDateTime getPostTime() {
+        return postTime;
+    }
+    public void setPostTime(LocalDateTime postTime) {
+        this.postTime = postTime;
+    }
+
+}
