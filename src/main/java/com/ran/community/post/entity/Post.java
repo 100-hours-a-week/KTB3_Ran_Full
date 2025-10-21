fdab107babd9c904df76ab7dbaf5ea2114cf9930
happy7yong <happy7yong83@gmail.com>
2025-10-21 23:10:22 +0900

    refactory : post setter 삭제

--- src/main/java/com/ran/community/post/entity/Post.java
+++ src/main/java/com/ran/community/post/entity/Post.java
@@ -3,69 +3,47 @@ package com.ran.community.post.entity;
 import com.ran.community.user.entity.User;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class Post {
     private long postId;
     private String postTitle;
     private String postContent;
     private long postAuthor;//User에게서 가져오기 FK
-    private String postImageUrl;
+    private Optional<String> postImageUrl;
     private LocalDateTime postDate;
 
-    public Post() {}
-    public Post(long postId, String postTitle, String postContent, User user, LocalDateTime postDate, String postImageUrl) {
+    public Post(long postId, String postTitle, String postContent, long postAuthor, String postImageUrl) {
         this.postId = postId;
         this.postTitle = postTitle;
         this.postContent = postContent;
-        this.postAuthor = user.getUserId(); //이게 맞나?
-        this.postDate = postDate;
-        this.postImageUrl = postImageUrl;
+        this.postAuthor = postAuthor; //이게 맞나?
+        this.postDate = LocalDateTime.now();
+        this.postImageUrl = Optional.ofNullable(postImageUrl);
     }
 
     public long getPostId() {
         return postId;
     }
 
-    public void setPostId(long postId) {
-        this.postId = postId;
-    }
-
     public String getPostTitle() {
         return postTitle;
     }
 
-    public void setPostTitle(String postTitle) {
-        this.postTitle = postTitle;
-    }
 
     public String getPostContent() {
         return postContent;
-    }
-
-    public void setPostContent(String postContent) {
-        this.postContent = postContent;
     }
 
     public long getPostAuthor() {
         return postAuthor;
     }
 
-    public void setPostAuthor(long postAuthor) {
-        this.postAuthor = postAuthor;
-    }
-
     public LocalDateTime getPostDate() {
         return postDate;
     }
 
-    public void setPostDate() {
-        this.postDate = LocalDateTime.now();
-    }
-
-    public String getPostImageUrl() {
+    public Optional<String> getPostImageUrl() {
         return postImageUrl;
-    }
-    public void setPostImageUrl(String postImageUrl) {
-        this.postImageUrl = postImageUrl;
     }
 }
