6a00638b89737718877829fc6b463b8bc0e42aee
happy7yong <happy7yong83@gmail.com>
2025-10-21 22:52:36 +0900

    refactory : User setter 삭제

--- src/main/java/com/ran/community/user/entity/User.java
+++ src/main/java/com/ran/community/user/entity/User.java
@@ -7,7 +7,6 @@ public class User {
     private String username;
     private String password;
 
-    public User() {}
     public User(long userId, String email, String username, String password) {
         this.userId = userId;
         this.email = email;
@@ -19,32 +18,16 @@ public class User {
         return userId;
     }
 
-    public void setUserId(long userId){
-        this.userId = userId;
-    }
-
     public String getEmail(){
         return email;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
     }
 
     public String getUsername(){
         return username;
     }
 
-    public void setUsername(String username){
-        this.username = username;
-    }
-
     public String getPassword(){
         return password;
-    }
-
-    public void setPassword(String password){
-        this.password=password;
     }
 
     public String toString(){
