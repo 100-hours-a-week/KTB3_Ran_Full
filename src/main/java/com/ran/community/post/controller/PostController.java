7fba101ef1db9a6efb2a00f50aa0bf10511ae429
happy7yong <happy7yong83@gmail.com>
2025-10-22 20:01:32 +0900

    refactory : like DTO로 변경 후 apiResponse로 반환

--- src/main/java/com/ran/community/post/controller/PostController.java
+++ src/main/java/com/ran/community/post/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.ran.community.post.controller;
 
+import com.ran.community.global.ApiResponse;
 import com.ran.community.like.dto.response.LikeCountDto;
+import com.ran.community.like.dto.response.LikeDataDto;
 import com.ran.community.like.entity.Like;
 import com.ran.community.like.service.LikeService;
 import com.ran.community.post.dto.response.PageDto;
@@ -34,67 +36,68 @@ public class PostController {
 
     //게시물 생성 //✅
     @PostMapping()
-    public ResponseEntity<?> postCreate(@Valid @RequestBody PostCreateFormDto postCreateFormDto, HttpSession httpSession) {
+    public ResponseEntity<ApiResponse<PostDataDto>> postCreate(@Valid @RequestBody PostCreateFormDto postCreateFormDto, HttpSession httpSession) {
         //userId의 userDto를 가져오기
         User user = userService.getUser((long) httpSession.getAttribute("userId"));
         //로그인된 user의 객체도 함께 사용
-        postService.postCreate(user,postCreateFormDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message","posting_success"));
+        PostDataDto postDataDto= postService.postCreate(user,postCreateFormDto);
+        return ApiResponse.success(postDataDto,"created_post");
     }
 
     //특정 게시물 조회 //✅
     @GetMapping("/{postId}")
-    public ResponseEntity<?> postRead(@PathVariable Long postId){
-        PostDataDto post = postService.postReadData(postId);
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","posting_read_success","data", post));
+    public ResponseEntity<ApiResponse<PostDataDto>> postRead(@PathVariable Long postId){
+        PostDataDto post = postService.postRead(postId);
+        return ApiResponse.success(post,"read_post");
     }
 
     //전체 게시물 조회 //✅
     @GetMapping()
-    public ResponseEntity<?> postsRead(HttpSession httpSession,@RequestParam int page, @RequestParam int limit){
+    public ResponseEntity<ApiResponse<PageDto>> postsRead(HttpSession httpSession,@RequestParam int page, @RequestParam int limit){
         //userId가 있을 경우에만 확인
         userService.getUser((long) httpSession.getAttribute("userId"));
         PageDto pageData = postService.postsRead(page,limit);//페이지 네이션 되어있음.
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","posts_read_success","data",pageData.getOffsetPosts(),"meta",pageData.getPageMeta()));
-
+        return ApiResponse.success(pageData,"read_all_post");
     }
 
     //게시물 수정 //✅
     @PatchMapping("/{postId}")
-    public ResponseEntity<?> updatePost(@Valid @RequestBody PostCreateFormDto postCreateFormDto, @PathVariable Long postId){
-        postService.updatePost(postId,postCreateFormDto);
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","posts_patch_success"));
+    public ResponseEntity<ApiResponse<PostDataDto>> updatePost(@Valid @RequestBody PostCreateFormDto postCreateFormDto, @PathVariable Long postId){
+        PostDataDto postDataDto = postService.updatePost(postId,postCreateFormDto);
+        return ApiResponse.success(postDataDto,"update_post");
     }
 
     //게시물 삭제 //✅
     @DeleteMapping("/{postId}")
-    public ResponseEntity<?> deletePost(@PathVariable Long postId){
-        postService.deletePost(postId);
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","posts_delete_success"));
+    public ResponseEntity<ApiResponse<PostDataDto>> deletePost(@PathVariable Long postId){
+        PostDataDto postDataDto = postService.deletePost(postId);
+        return ApiResponse.success(postDataDto,"delete_post");
     }
 
-    //특정 게시물의 좋아요 생성 및 삭제
+
+    //좋아요를 분리해야되나?
+    //특정 게시물의 좋아요 생성
     @PostMapping("/{postId}/likes")
-    public ResponseEntity<?> like(@PathVariable Long postId, HttpSession session) {
+    public ResponseEntity<ApiResponse<LikeDataDto>> like(@PathVariable Long postId, HttpSession session) {
         long userId = (long) session.getAttribute("userId");
-        likeService.addLike(userId,postId);
-        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message","like_create_success"));
+        LikeDataDto likeDataDto =likeService.addLike(userId,postId);
+        return ApiResponse.success(likeDataDto,"like");
     }
 
     //특정 게시물의 좋아요 삭제
     @DeleteMapping("/{postId}/likes")
-    public ResponseEntity<?> unlike(@PathVariable Long postId, HttpSession session) {
+    public ResponseEntity<ApiResponse<LikeDataDto>> unlike(@PathVariable Long postId, HttpSession session) {
         long userId = (long) session.getAttribute("userId");
-        likeService.removeLike(userId,postId);
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","unlike_success"));
+        LikeDataDto likeDataDto = likeService.removeLike(userId,postId);
+        return ApiResponse.success(likeDataDto, "unlike");
 
     }
 
     //특정 게시물의 좋아요 조회
     @GetMapping("/{postId}/likes")
-    public ResponseEntity<?> getLikes(@PathVariable Long postId) {
+    public ResponseEntity<ApiResponse<LikeCountDto>> getLikes(@PathVariable Long postId) {
         LikeCountDto likeCount = likeService.getLikeCountDto(postId);
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","like_read_success","data",likeCount));
+        return ApiResponse.success(likeCount,"get_like");
     }
 
 }
