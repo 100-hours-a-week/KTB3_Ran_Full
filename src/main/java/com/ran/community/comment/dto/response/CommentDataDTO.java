f8d9bdc9dfc338f85a836738cd81dce2e04c041e
happy7yong <happy7yong83@gmail.com>
2025-10-22 20:05:00 +0900

    refactory:Dto에 생성자 어뎁터 추가

--- src/main/java/com/ran/community/comment/dto/response/CommentDataDTO.java
+++ src/main/java/com/ran/community/comment/dto/response/CommentDataDTO.java
@@ -4,17 +4,24 @@ import com.ran.community.comment.entity.Comment;
 
 import java.time.LocalDateTime;
 
-public class CommentResponseDTO {
+public class CommentDataDTO {
     private long commentId;
     private String content;
     private long authorId;
     private LocalDateTime postTime;
 
-    public CommentResponseDTO(long commentId, String content, long authorId, LocalDateTime postTime) {
+    public CommentDataDTO(long commentId, String content, long authorId, LocalDateTime postTime) {
         this.commentId = commentId;
         this.content = content;
         this.authorId = authorId;
         this.postTime = postTime;
+    }
+
+    public CommentDataDTO(Comment comment) {
+        this.commentId = comment.getCommentId();
+        this.content = comment.getContent();
+        this.authorId = comment.getAuthorId();
+        this.postTime = comment.getPostTime();
     }
 
     public long getCommentId() {
