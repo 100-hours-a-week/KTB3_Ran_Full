8e0cbbc6198df38fe82bc4407fce2cc0fd9fb517
happy7yong <happy7yong83@gmail.com>
2025-10-22 13:57:18 +0900

    refactory : entity setter 삭제

--- src/main/java/com/ran/community/comment/entity/Comment.java
+++ src/main/java/com/ran/community/comment/entity/Comment.java
@@ -12,43 +12,27 @@ public class Comment {
     private long postId;
     private LocalDateTime postTime;
 
-    public Comment() {}
-    public Comment(long commentId, String content, User user, Post post, LocalDateTime postTime) {
+    public Comment(long commentId, String content, long authorId, long postId) {
         this.commentId = commentId;
         this.content = content;
-        this.authorId = user.getUserId();
-        this.postId = post.getPostId();
+        this.authorId = authorId;
+        this.postId = postId;
         this.postTime = LocalDateTime.now();
     }
 
     public long getCommentId() {
         return commentId;
     }
-    public void setCommentId(long commentId) {
-        this.commentId = commentId;
-    }
     public String getContent() {
         return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
     }
     public long getAuthorId() {
         return authorId;
     }
-    public void setAuthorId(long authorId) {
-        this.authorId = authorId;
-    }
     public Long getPostId() {
         return postId;
     }
-    public void setPostId(long postId) {
-        this.postId = postId;
-    }
     public LocalDateTime getPostTime() {
         return postTime;
-    }
-    public void setPostTime() {
-        this.postTime = LocalDateTime.now();
     }
 }
