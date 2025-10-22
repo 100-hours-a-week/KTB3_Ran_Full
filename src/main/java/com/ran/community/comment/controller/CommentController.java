ca15fac55cf8b010223c685c293df5fa3aea9e35
happy7yong <happy7yong83@gmail.com>
2025-10-22 20:14:36 +0900

    refactory: ApiResponse로 부터 가져옴

--- src/main/java/com/ran/community/comment/controller/CommentController.java
+++ src/main/java/com/ran/community/comment/controller/CommentController.java
@@ -1,9 +1,9 @@
 package com.ran.community.comment.controller;
 
-import com.ran.community.comment.dto.response.CommentResponseDTO;
-import com.ran.community.comment.entity.Comment;
+import com.ran.community.comment.dto.response.CommentDataDTO;
 import com.ran.community.comment.dto.request.CommentInputDto;
 import com.ran.community.comment.service.CommentService;
+import com.ran.community.global.ApiResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,32 +24,32 @@ public class CommentController {
 
     //댓글 생성
     @PostMapping()
-    public ResponseEntity<?> commentCreate(HttpSession session, @PathVariable Long postId, @RequestBody CommentInputDto commentInputDto) {
+    public ResponseEntity<ApiResponse<CommentDataDTO>> commentCreate(HttpSession session, @PathVariable Long postId, @RequestBody CommentInputDto commentInputDto) {
         long userId = (long) session.getAttribute("userId");
-        commentService.commentCreate(userId,postId,commentInputDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message","comment_success"));
+        CommentDataDTO commentDataDTO = commentService.commentCreate(userId,postId,commentInputDto);
+        return ApiResponse.created(commentDataDTO,"comment_created_success");
     }
 
     //특정 게시글에서 댓글 조회
     @GetMapping()
-    public ResponseEntity<?> commentList(@PathVariable Long postId) {
-        List<CommentResponseDTO> commentList = commentService.commentListByPostId(postId);
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","comment_list_success","data",commentList));
+    public ResponseEntity<ApiResponse<List<CommentDataDTO>>> commentList(@PathVariable Long postId) {
+        List<CommentDataDTO> commentList = commentService.commentListByPostId(postId);
+        return ApiResponse.success(commentList, "List of comments_success");
     }
 
     //댓글 수정
     @PatchMapping("/{commentId}")
     public ResponseEntity<?> commentUpdate(HttpSession session, @PathVariable Long postId,@PathVariable Long commentId, @RequestBody CommentInputDto commentInputDto) {
         long userId = (long) session.getAttribute("userId");
-        commentService.commentUpdate(userId,postId,commentId,commentInputDto);
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","comment_update_success"));
+        CommentDataDTO commentDataDTO = commentService.commentUpdate(userId,postId,commentId,commentInputDto);
+        return ApiResponse.success(commentDataDTO,"comment_updated_success");
     }
 
     //댓글 삭제
     @DeleteMapping("/{commentId}")
     public ResponseEntity<?> commentDelete(HttpSession session, @PathVariable Long postId,@PathVariable Long commentId) {
         long userId = (long) session.getAttribute("userId");
-        commentService.commentDelete(userId,postId,commentId);
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","comment_delete_success"));
+        CommentDataDTO commentDataDTO = commentService.commentDelete(userId,postId,commentId);
+        return ApiResponse.success(commentDataDTO,"comment_deleted_success");
     }
 }
