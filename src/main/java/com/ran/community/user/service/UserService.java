d8cf5b894387763395ed26cb316080fb22f21700
happy7yong <happy7yong83@gmail.com>
2025-10-22 20:11:23 +0900

    refactory: 접근제어자 내부에서만 사용하는 메서드는 public -> private으로 변경

--- src/main/java/com/ran/community/user/service/UserService.java
+++ src/main/java/com/ran/community/user/service/UserService.java
@@ -27,29 +27,29 @@ public class UserService {
     }
 
     //이메일과 닉네임을 비교
-    public boolean equalUserInfo(User user, UserLoginDto userLoginDto) {
+    private boolean equalUserInfo(User user, UserLoginDto userLoginDto) {
         return user.getPassword().equals(userLoginDto.getPassword());
     }
 
-    //식별자로 유저 찾기
+    //식별자로 유저 찾기 //해당 메서드는 외부에서 사용할 수도 있음.
     public User getUser(long userId){
         return userRepository.getUser(userId).orElseThrow(()-> new IllegalArgumentException("유저를 찾을 수 없습니다."));
     }
 
     //유저 생성
-    public User createUser(UserSignupFormDto userSignupFormDto){
+    private User createUser(UserSignupFormDto userSignupFormDto){
         return userRepository.addUser(userSignupFormDto);
     }
 
     //닉네임 중복
-    public void duplicateUsername(UserSignupFormDto userSignupFormDto){
+    private void duplicateUsername(UserSignupFormDto userSignupFormDto){
         userRepository.findByUsername(userSignupFormDto.getUsername()).ifPresent(user -> {
             throw new IllegalArgumentException("중복된 닉네임 입니다.");
         });
     }
 
     //이메일 중복
-    public void duplicateEmail(UserSignupFormDto userSignupFormDto){
+    private void duplicateEmail(UserSignupFormDto userSignupFormDto){
         userRepository.findByEmail(userSignupFormDto.getEmail()).ifPresent(user ->{
             throw new IllegalArgumentException("중복된 이메일 입니다.");
         });
@@ -67,7 +67,7 @@ public class UserService {
     }
 
     //비밀번호 재 확인
-    public void passwordConfirm(UserSignupFormDto userSignupFormDto){
+    private void passwordConfirm(UserSignupFormDto userSignupFormDto){
         if(!userSignupFormDto.getPassword().equals(userSignupFormDto.getConfirmPassword())){
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
@@ -75,7 +75,7 @@ public class UserService {
 
     //유저가 존재하는지 확인
     //로그인
-    public User userExists(UserLoginDto userLoginDto) {
+    private User userExists(UserLoginDto userLoginDto) {
         User user = userRepository.findByEmail(userLoginDto.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("아이디 또는 비밀번호를 확인해주세요."));
 
