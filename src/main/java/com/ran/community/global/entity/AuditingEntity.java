cfe776d79f8e12cd5a4099f1da5f37510cbafb6c
happy7yong <happy7yong83@gmail.com>
2025-11-01 10:02:44 +0900

    feat : created, updated 감사로그 엔티티 생성

---
+++ src/main/java/com/ran/community/global/entity/AuditingEntity.java
@@ -0,0 +1,33 @@
+package com.ran.community.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) //감사 리스너 등록
+@EnableJpaAuditing
+public abstract class AuditingEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime updated_at;
+
+    @LastModifiedDate
+    @Column(nullable = true, updatable = false)
+    private LocalDateTime deleted_at;
+
+}
