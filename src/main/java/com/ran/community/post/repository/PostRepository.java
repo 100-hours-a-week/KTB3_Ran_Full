3904322aa9e85c9f5105b52d77af716e6a810eac
happy7yong <happy7yong83@gmail.com>
2025-11-05 15:17:34 +0900

    pref : DTO projection

--- src/main/java/com/ran/community/post/repository/PostRepository.java
+++ src/main/java/com/ran/community/post/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.ran.community.post.repository;
 
 import com.ran.community.post.dto.request.PostCreateFormDto;
+import com.ran.community.post.dto.response.PostCountDto;
 import com.ran.community.post.entity.Post;
 import com.ran.community.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,6 +25,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("SELECT p.viewCount FROM Post p WHERE p.id = :postId")
     int findViewCountByPostId(@Param("postId") long postId);
 
+    //좋아요 댓글 조회수 갯수 모두 한꺼번에 가져오기 //DTO projection 사용하기
+    @Query("SELECT new com.ran.community.post.dto.response.PostCountDto(p.viewCount, p.commentCount, p.likeCount) FROM Post p WHERE p.id = :postId")
+    PostCountDto findCountByPostId(@Param("postId") long postId);
 
     //postId에 대해 댓글 조회하기
     //post 테이블 데이터에서 댓글을 가지고 올건데, postId값인 id에서 p(전체)를 가져올것임.
