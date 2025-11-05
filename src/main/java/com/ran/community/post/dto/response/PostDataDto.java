bc0174d3df284564176d7959ed01a8b0880d9266
happy7yong <happy7yong83@gmail.com>
2025-11-05 14:53:07 +0900

    feat : post 상세보기

--- src/main/java/com/ran/community/post/dto/response/PostDataDto.java
+++ src/main/java/com/ran/community/post/dto/response/PostDataDto.java
@@ -1,11 +1,15 @@
 package com.ran.community.post.dto.response;
 
 
+import com.ran.community.comment.dto.response.CommentDataDTO;
+import com.ran.community.comment.entity.Comment;
 import com.ran.community.post.entity.Post;
 import com.ran.community.user.entity.User;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Getter
@@ -13,8 +17,10 @@ public class PostDataDto {
     private long id;
     private String title;
     private String content;
-    private String postAuthor;//User에게서 가져오기 FK //게시물 만든이
+    private String postAuthor;
     private String imgUrl;
+    private PostCountDto Count;//있는거 재활 가능하지루
+    private List<CommentDataDTO> comments ; //댓글 리스트
 
     public PostDataDto(Post post) {
         this.id = post.getId();
@@ -22,5 +28,7 @@ public class PostDataDto {
         this.content = post.getContent();
         this.postAuthor = post.getUser().getUsername();
         this.imgUrl = post.getImgUrl();
+        this.Count = new PostCountDto(post.getCommentCount(), post.getLikeCount(), post.getViewCount());
+        this.comments = post.getComments().stream().map(comment -> new CommentDataDTO(comment)).toList();
     }
 }
