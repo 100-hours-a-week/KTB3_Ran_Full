a3379cb51f83fb2a651711ea34242c4bccac34e4
happy7yong <happy7yong83@gmail.com>
2025-11-05 15:57:23 +0900

    refactory : Dto 명 일관성

--- src/main/java/com/ran/community/post/dto/response/PostDataDto.java
+++ src/main/java/com/ran/community/post/dto/response/PostDataDto.java
@@ -1,16 +1,11 @@
 package com.ran.community.post.dto.response;
 
 
-import com.ran.community.comment.dto.response.CommentDataDTO;
-import com.ran.community.comment.entity.Comment;
+import com.ran.community.comment.dto.response.CommentDataDto;
 import com.ran.community.post.entity.Post;
-import com.ran.community.user.entity.User;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Getter
 public class PostDataDto {
@@ -20,7 +15,7 @@ public class PostDataDto {
     private String postAuthor;
     private String imgUrl;
     private PostCountDto Count;//있는거 재활 가능하지루
-    private List<CommentDataDTO> comments ; //댓글 리스트
+    private List<CommentDataDto> comments ; //댓글 리스트
 
     public PostDataDto(Post post) {
         this.id = post.getId();
@@ -29,6 +24,6 @@ public class PostDataDto {
         this.postAuthor = post.getUser().getUsername();
         this.imgUrl = post.getImgUrl();
         this.Count = new PostCountDto(post.getCommentCount(), post.getLikeCount(), post.getViewCount());
-        this.comments = post.getComments().stream().map(comment -> new CommentDataDTO(comment)).toList();
+        this.comments = post.getComments().stream().map(comment -> new CommentDataDto(comment)).toList();
     }
 }
