37ec99977c31e9fb37194537aa4bea893de4ca29
happy7yong <happy7yong83@gmail.com>
2025-11-13 17:06:45 +0900

    feat : login 시 쿠키 저장

--- src/main/java/com/ran/community/global/config/WebConfig.java
+++ src/main/java/com/ran/community/global/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500") // 프론트 주소
+                        .allowedOrigins("http://127.0.0.1:5500","http://localhost:5500") // 프론트 주소
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
