c954dcf9b8002c76be585156e024faf4bcf3ae9a
happy7yong <happy7yong83@gmail.com>
2025-11-15 11:42:39 +0900

    feat : 게시글 수정 api 연결

--- src/main/java/com/ran/community/global/config/WebConfig.java
+++ src/main/java/com/ran/community/global/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5500") // 프론트 주소
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                         .allowedHeaders("*")
                         .exposedHeaders("Set-Cookie")
                         .allowCredentials(true);
